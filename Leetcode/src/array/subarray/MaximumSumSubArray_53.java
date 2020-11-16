9bb5714b64f756d92e5fe544268053a192ddeea1
unknown <abhinavkumar08@gmail.com>
2020-11-16 23:40:09 +0700

    Maximum Sum and shortest subarray to be removed to make array sorted

--- Leetcode/src/array/subarray/MaximumSumSubArray_53.java
+++ Leetcode/src/array/subarray/MaximumSumSubArray_53.java
@@ -1,5 +1,46 @@
 package array.subarray;
 
 public class MaximumSumSubArray_53 {
+	
+	
+	 public int maxSubArrayKadane(int[] nums) {
+	        
+	        int sumSoFar = 0;
+	        int maxSum = Integer.MIN_VALUE;
+	        int start=0; int end=0;
+	        
+	        for(int i=0; i<nums.length; i++){
+	            sumSoFar += nums[i];
+	            
+	            if(sumSoFar<0){
+	                start = i+1;
+	                sumSoFar = 0;
+	            }
+	            
+	            if(sumSoFar > maxSum){
+	                maxSum = sumSoFar;
+	                end = i;
+	            }
+	        }
+	        
+	        System.out.println("i "+start);
+	        System.out.println("j "+end);
+	        return maxSum;
+	        
+	   }
+	 
+	 
+	 public int maxSubArray(int[] nums) {
+	        
+	       int sumSoFar = 0;
+	        int maxSum = Integer.MIN_VALUE;
+	        
+	        for(int i=0; i<nums.length; i++){
+	            sumSoFar =  Math.max(nums[i], nums[i]+sumSoFar);
+	            maxSum = Math.max(maxSum, sumSoFar);
+	        }
+	        
+	        return maxSum;
+	    }
 
 }
---
+++ Leetcode/src/array/subarray/ShortestSubarrayToBeRemovedToMakeArraySorted.java
@@ -0,0 +1,29 @@
+package array.subarray;
+
+public class ShortestSubarrayToBeRemovedToMakeArraySorted {
+	
+	 public int findLengthOfShortestSubarray(int[] arr) {
+	        
+	        int n = arr.length;
+	        int i = 0;
+	        int j = n-1;
+	        
+	        while(i<=n-2 && arr[i]<=arr[i+1])i++;
+	        while(j>=1 && arr[j]>=arr[j-1])j--;
+	        
+	        if(i==n-1)return 0;
+	        if(j==0) return 1;
+	        int res =  Math.min(n-i-1, j);
+	        int start = 0;
+	        int end = j;
+	        
+	        while(start<=i && end<n){
+	            if(arr[start]<=arr[end]){
+	                start++;
+	                res = Math.min(res, end-start);
+	            }else
+	                end++;
+	        }
+	        return res;
+	    }
+}
